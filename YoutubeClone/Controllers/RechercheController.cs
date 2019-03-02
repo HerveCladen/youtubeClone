8b504089dc9ecd2a70d232d43d79449af94cd165
Herve Claden <hervecladen@gmail.com>
2019-03-01 23:10:36 -0500

    Algorithme de recherche de David ajouté + Fix barre de recherche

--- YoutubeClone/Controllers/RechercheController.cs
+++ YoutubeClone/Controllers/RechercheController.cs
@@ -6,58 +6,88 @@ using System.Web.Mvc;
 using YoutubeClone.Models.Data_Models;
 using PagedList;
 using PagedList.Mvc;
+using YoutubeClone.Models.View_Models;
 
 namespace YoutubeClone.Controllers
 {
-    public class RechercheController : Controller {
+    public class RechercheController : Controller
+    {
         private YoutubeCloneDb db = new YoutubeCloneDb();
         // GET: Recherche
-        
-        public ActionResult Index(bool isFromCat = false, string categorie = "All",string search="",string isChaine="off", string isVideo = "off", string uploadFrame="All",string sortBy="Popular", int page=1) {
-           /*
-            * page est automatique
-            * video/chaine peuvent etre false ou true
-            *categorie doit etre la partie de l'enum
-            *sort by defaut cest popular sinon upload 
-            *upload date prend all/day/week/month/year et s'automatise
-            * test le match dans le title/desc/tags
-            */
-            if (search != "" || isFromCat) {
+
+        public ActionResult Index(bool isFromCat = false, string categorie = "All", string search = "", string isChaine = "on", string isVideo = "on", string uploadFrame = "All", string sortBy = "Popular", int page = 1)
+        {
+            /*
+             * page est automatique
+             * video/chaine peuvent etre false ou true
+             *categorie doit etre la partie de l'enum
+             *sort by defaut cest popular sinon upload 
+             *upload date prend all/day/week/month/year et s'automatise
+             * test le match dans le title/desc/tags
+             */
+            if (search != "" || isFromCat)
+            {
                 var Videos = db.Videos;
                 var Chaines = db.Chaines;
                 IQueryable<Video> queriedVideos;
                 IQueryable<Chaine> queriedChaines;
 
-                queriedVideos = Videos.Where(V=>V.Name.Contains(search) || V.Tags_Video.Contains(search) ||V.Description.Contains(search));
-                queriedChaines = Chaines.Where(C=>C.Name.Contains(search) || C.Tags_Chaine.Contains(search) || C.Description.Contains(search));
+                queriedVideos = Videos.Where(V => V.Name.Contains(search) || V.Tags_Video.Contains(search) || V.Description.Contains(search));
+                queriedChaines = Chaines.Where(C => C.Name.Contains(search) || C.Tags_Chaine.Contains(search) || C.Description.Contains(search));
 
-                if (categorie != "All") {
+                if (categorie != "All")
+                {
                     queriedVideos = queriedVideos.Where(V => V.Categorie_Video.ToString() == categorie);
-                    queriedChaines = queriedChaines.Where(C=>C.Categorie_Chaine.ToString() == categorie);
+                    queriedChaines = queriedChaines.Where(C => C.Categorie_Chaine.ToString() == categorie);
                 }
 
-                if (sortBy == "Popular") {
-                    queriedVideos = queriedVideos.OrderByDescending(C => C.Views);
-                    queriedChaines = queriedChaines.OrderByDescending(C => C.Videos.Sum(t => t.Views));
-                } else if (sortBy == "UploadDate") {
+                if (sortBy == "UploadDate")
+                {
                     queriedVideos = queriedVideos.OrderByDescending(C => C.DatePublished);
                     queriedChaines = queriedChaines.OrderByDescending(C => C.Videos.Max(t => t.DatePublished));
-                } else {
-                    //sort by popularity by default
+                }
+                else if (sortBy == "Relevance" && !isFromCat)
+                {
+                    //sort by relevance but only if it does not come from category as it would mean the search parameter is empty
+                    List<RechercheCoefficient> videoCoefficients = new List<RechercheCoefficient>();
+                    foreach (Video v in queriedVideos)
+                    {
+                        videoCoefficients.Add(new RechercheCoefficient(search, v.Name, v.Description, v.Tags_Video, v.VideoId));
+                    }
+                    List<RechercheCoefficient> chaineCoefficients = new List<RechercheCoefficient>();
+                    foreach (Chaine v in queriedChaines)
+                    {
+                        chaineCoefficients.Add(new RechercheCoefficient(search, v.Name, v.Description, v.Tags_Chaine, v.ChaineId));
+                    }
+                    videoCoefficients.OrderByDescending(v => v.coefficient);
+                    queriedVideos = queriedVideos.ToList().OrderBy(C => videoCoefficients.IndexOf(videoCoefficients.Find(A => A.id == C.VideoId))).ThenBy(C => C.Views).ToList().AsQueryable();
+                    queriedChaines = queriedChaines.ToList().OrderBy(C => chaineCoefficients.IndexOf(chaineCoefficients.Find(A => A.id == C.ChaineId))).ThenBy(C => C.Videos.Sum(t => t.Views)).ToList().AsQueryable();
+                }
+                else
+                {
+                    //sortby popular by default or if comes from category while relevance is manually put in
                     queriedVideos = queriedVideos.OrderByDescending(C => C.Views);
                     queriedChaines = queriedChaines.OrderByDescending(C => C.Videos.Sum(t => t.Views));
                 }
 
 
-                if (uploadFrame != "All") {
+                if (uploadFrame != "All")
+                {
                     DateTime CompareDate = DateTime.Today;
-                    if (uploadFrame == "Day") {
+                    if (uploadFrame == "Day")
+                    {
                         CompareDate.AddDays(-1);
-                    } else if (uploadFrame == "Week") {
+                    }
+                    else if (uploadFrame == "Week")
+                    {
                         CompareDate.AddDays(-7);
-                    } else if (uploadFrame == "Month") {
+                    }
+                    else if (uploadFrame == "Month")
+                    {
                         CompareDate.AddDays(-30);
-                    } else if (uploadFrame == "Year") {
+                    }
+                    else if (uploadFrame == "Year")
+                    {
                         CompareDate.AddDays(-365);
                     }
                     queriedVideos = queriedVideos.Where(C => C.DatePublished > CompareDate);
@@ -75,17 +105,21 @@ namespace YoutubeClone.Controllers
                     ViewBag.StatusChaine = true;
                 else
                     ViewBag.StatusChaine = false;
-                ViewBag.Recherche = search+"  of type " +categorie+"  within "+ uploadFrame+"  sorted by "+sortBy;
-                if (!(isVideo.ToUpper()=="ON")) {
+                ViewBag.Recherche = search + "  of type " + categorie + "  within " + uploadFrame + "  sorted by " + sortBy;
+                if (!(isVideo.ToUpper() == "ON"))
+                {
                     ViewBag.Videos = new List<Video>();
                     ViewBag.StatusVideo = false;
                 }
-                if (!(isChaine.ToUpper()== "ON")) {
+                if (!(isChaine.ToUpper() == "ON"))
+                {
                     ViewBag.Chaines = new List<Chaine>();
                     ViewBag.StatusChaine = false;
                 }
                 return View("~/Views/Recherche/Recherche.cshtml");
-            } else {
+            }
+            else
+            {
                 ViewBag.StatusVideo = false;
                 ViewBag.StatusChaine = false;
                 ViewBag.Videos = new List<Video>();
@@ -93,6 +127,6 @@ namespace YoutubeClone.Controllers
                 return View("~/Views/Recherche/Recherche.cshtml");
             }
         }
-        
+
     }
 }
---
+++ YoutubeClone/Models/View_Models/RechercheCoefficient.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace YoutubeClone.Models.View_Models
+{
+    public class RechercheCoefficient
+    {
+        //Search parameters
+        public int id;
+        public string searchQuery;
+        public string itemName;
+        public string itemDescription;
+        public string itemTags;
+
+        //Total coefficient
+        public double coefficient = 0.001;
+        //Amount of points for each matches as tiebreaker/troubleshooting help
+        public double MatchesName = 0;
+        public double MatchesDescription = 0;
+        public double MatchesTags = 0;
+        //Variables to tune the algorythm
+        private double coefficientName = 1.5;
+        private double coefficientDescription = 1.25;
+        private double coefficientTags = 1.0;
+        private string[] keywords;
+
+        public RechercheCoefficient(string searchQuery, string itemName, string itemDescription, string itemTags, int id)
+        {
+            this.searchQuery = searchQuery;
+            this.itemName = itemName;
+            this.itemDescription = itemDescription;
+            this.itemTags = itemTags;
+            this.id = id;
+
+            keywords = searchQuery.Split(' ');
+
+            foreach (string keyword in keywords)
+            {
+                MatchesName += CountPoints(itemName, keyword);
+                if (itemTags != null)
+                    MatchesTags += CountPoints(itemTags, keyword);
+                MatchesDescription += CountPoints(itemDescription, keyword);
+            }
+
+            coefficient = MatchesDescription * coefficientDescription + MatchesName * coefficientName + MatchesTags * coefficientTags;
+        }
+
+        private static double CountPoints(string itemText, string keyword)
+        {
+            // find every occurences of keyword
+            double points = 0;
+            int i = 0;
+            //Points per keyword size
+            double pointSmall = 1;
+            double pointMedium = 1.5;
+            double pointLarge = 2.0;
+            //Defined sizes
+            int sizeSmall = 3;
+            int sizeLarge = 8;
+
+            while ((i = itemText.IndexOf(keyword, i)) != -1)
+            {
+                i += keyword.Length;
+                //apply point according to keyword size
+                if (keyword.Length <= sizeSmall)
+                    points += pointSmall;
+                else if (keyword.Length <= sizeLarge)
+                    points += pointMedium;
+                else
+                    points += pointLarge;
+            }
+            return points;
+        }
+    }
+}
