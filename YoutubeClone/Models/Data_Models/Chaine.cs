c410136faf41bd666001028362aa7dd43fe18478
Herve Claden <hervecladen@gmail.com>
2019-03-05 01:14:41 -0500

    Fix Formatting Models
    Changements additionnels sur le formattage des DataAnnotations des models pour les rendre plus consistentes.

--- YoutubeClone/Models/Data_Models/Chaine.cs
+++ YoutubeClone/Models/Data_Models/Chaine.cs
@@ -13,18 +13,29 @@ namespace YoutubeClone.Models.Data_Models {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ChaineId { get; set; }
-        [MinLength(5), MaxLength(40),DisplayName("Entre le nom de la chaine"), Index(IsUnique = true)]
+
+        [StringLength(40, MinimumLength = 5)]
+        [DisplayName("Entre le nom de la chaine")]
+        [Index(IsUnique = true)]
         public string Name { get; set; }
-        [Required, MaxLength(180), DisplayName("Entre la description de la chaine")]
+
+        [Required]
+        [MaxLength(180)]
+        [DisplayName("Entre la description de la chaine")]
         [RegularExpression(@"(\S*\s){2,}(\S+){1}")]
         public string Description { get; set; }
+
         [ForeignKey("Utilisateur")]
         public int Utilisateur_FK { get; set; }
+
         [ForeignKey("Utilisateur_FK")]
         public virtual Utilisateur Utilisateur { get; set; }
+
         [Required]
         public Categorie Categorie_Chaine { get; set; }
+
         public virtual ICollection<Video> Videos { get; set; }
+
         [MaxLength(1050)]
         [RegularExpression("(([A-Za-z0-9_ ]{2,20},){0,49}([A-Za-z0-9_ ]{2,20})){0,1}")]
         public string Tags_Chaine { get; set; }
