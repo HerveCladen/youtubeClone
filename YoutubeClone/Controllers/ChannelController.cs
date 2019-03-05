63f2f5b3cb29b191dedd40e69f01a60da20b4d72
David G <david.genois13@gmail.com>
2019-03-05 11:51:41 -0500

    channel create + nav + chaines user

--- YoutubeClone/Controllers/ChannelController.cs
+++ YoutubeClone/Controllers/ChannelController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using YoutubeClone.Models.Data_Models;
 using PagedList;
 using PagedList.Mvc;
+using System.Net;
+using System.Data.Entity;
 
 namespace YoutubeClone.Controllers
 {
@@ -37,10 +39,95 @@ namespace YoutubeClone.Controllers
 
         //Ne retourne aucune chaîne
         [Authorize]
-        public ActionResult ChainesUtilisateurs(Utilisateur u)
+        public ActionResult ChainesUtilisateurs()
         {
             var Chaines = db.Chaines;
-            return View("~/Views/Channel/ChainesUtilisateurs.cshtml", Chaines.Where(c => c.Utilisateur_FK == u.UtilisateurId));
+            var user = db.Utilisateurs.Where(C => C.Username == User.Identity.Name).FirstOrDefault().UtilisateurId;
+            return View("~/Views/Channel/ChainesUtilisateurs.cshtml", Chaines.Where(c => c.Utilisateur_FK == user));
+        }
+
+        // GET: Chaines/Create
+        public ActionResult Create() {
+            ViewBag.Utilisateur_FK = db.Utilisateurs.Where(C => C.Username == User.Identity.Name).FirstOrDefault().UtilisateurId;
+            return View();
+        }
+
+        // POST: Chaines/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Name,Description,Utilisateur_FK,Categorie_Chaine,Tags_Chaine")] Chaine chaine) {
+            if (ModelState.IsValid) {
+                db.Chaines.Add(chaine);
+                db.SaveChanges();
+                return RedirectToAction("ChainesUtilisateurs");
+            }
+
+            ViewBag.Utilisateur_FK = db.Utilisateurs.Where(C => C.Username == User.Identity.Name).FirstOrDefault().UtilisateurId;
+            return View("Create.cshtml",chaine);
+        }
+
+        // GET: Chaines/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chaine chaine = db.Chaines.Find(id);
+            if (chaine == null) {
+                return HttpNotFound();
+            }
+            ViewBag.Utilisateur_FK = new SelectList(db.Utilisateurs, "UtilisateurId", "Username", chaine.Utilisateur_FK);
+            return View("~/Views/Channel/Edit.cshtml",chaine);
+        }
+
+        // POST: Chaines/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include = "ChaineId,Name,Description,Utilisateur_FK,Categorie_Chaine,Tags_Chaine")] Chaine chaine) {
+            if (ModelState.IsValid) {
+                db.Entry(chaine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Utilisateur_FK = new SelectList(db.Utilisateurs, "UtilisateurId", "Username", chaine.Utilisateur_FK);
+            return View("~/Views/Channel/Edit.cshtml",chaine);
+        }
+
+        // GET: Chaines/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Chaine chaine = db.Chaines.Find(id);
+            if (chaine == null) {
+                return HttpNotFound();
+            }
+            return View("~/Views/Channel/Delete.cshtml",chaine);
+        }
+
+        // POST: Chaines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id) {
+            Chaine chaine = db.Chaines.Find(id);
+            db.Chaines.Remove(chaine);
+            db.SaveChanges();
+            return Redirect("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
