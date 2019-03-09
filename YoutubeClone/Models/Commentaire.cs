5d829454d7aee78de55087a9f8573a136156d030
Hervé Claden <hervecladen@gmail.com>
2019-03-08 22:56:39 -0500

    First commit - Entité Commentaire (non implémentée)

---
+++ YoutubeClone/Models/Commentaire.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace YoutubeClone.Models.Data_Models
+{
+    public class Commentaire
+    {
+        [Key]
+        public string CommentaireId { get; set; }
+
+        [Required]
+        [MaxLength(10000)]
+        public string Contenu { get; set; }
+
+        [Required]
+        [DisplayName("Date de publication")]
+        public DateTime DatePublication { get; set; }
+
+        [ForeignKey("Auteur")]
+        public int Chaine_FK { get; set; }
+
+        [ForeignKey("Chaine_FK")]
+        public virtual Chaine Auteur { get; set; }
+
+        public Commentaire()
+        {
+            DatePublication = DateTime.Now;
+        }
+    }
+}
