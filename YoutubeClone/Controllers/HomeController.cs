3ba5f5155a8dcfb68315f3c7ef65373b16f0f0c7
David <10902921+13DavidG@users.noreply.github.com>
2019-03-11 23:03:56 -0400

    Clear auth cookie on user delete

--- YoutubeClone/Controllers/HomeController.cs
+++ YoutubeClone/Controllers/HomeController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using YoutubeClone.Models.Data_Models;
 
 namespace YoutubeClone.Controllers
 {
     
-    public class HomeController : Controller
-    {
+    public class HomeController : Controller {
+        private YoutubeCloneDb db = new YoutubeCloneDb();
         // GET: Home
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated) {
+                if (db.Utilisateurs.Where(c => c.Username == User.Identity.Name).Count() == 0) {
+                    FormsAuthentication.SignOut();
+                    Session.Clear();
+                    return RedirectToAction("Index");
+                }
+            }
             return View();
         }
 
